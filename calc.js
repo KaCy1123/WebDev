e78193e0d8a8b236614909830b32ec562580d730
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2023-11-16 12:44:08 -0500

    Calculator beta

---
+++ calc.js
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+window.addEventListener("load",addListener);
+
+function addListener()
+{
+	document.getElementById("btnseven").addEventListener("click",inputseven);
+	document.getElementById("btneight").addEventListener("click",inputeight);
+	document.getElementById("btnnine").addEventListener("click",inputnine);
+	document.getElementById("btnfour").addEventListener("click",inputfour);
+	document.getElementById("btnfive").addEventListener("click",inputfive);
+	document.getElementById("btnsix").addEventListener("click",inputsix);
+	document.getElementById("btnone").addEventListener("click",inputone);
+	document.getElementById("btntwo").addEventListener("click",inputtwo);
+	document.getElementById("btnthree").addEventListener("click",inputthree);
+	document.getElementById("btnzero").addEventListener("click",inputzero);
+}
+function inputseven()
+{
+	seven = document.getElementById("btnseven").textContent;
+	document.getElementById("lblfullbtns").textContent += seven
+}
+function inputeight()
+{
+	eight = document.getElementById("btneight").textContent;
+	document.getElementById("lblfullbtns").textContent += eight
+}
+function inputnine()
+{
+	nine = document.getElementById("btnnine").textContent;
+	document.getElementById("lblfullbtns").textContent += nine
+}
+function inputfour()
+{
+	four = document.getElementById("btnfour").textContent;
+	document.getElementById("lblfullbtns").textContent += four
+}
+function inputfive()
+{
+	five = document.getElementById("btnfive").textContent;
+	document.getElementById("lblfullbtns").textContent += five
+}
+function inputsix()
+{
+	six = document.getElementById("btnsix").textContent;
+	document.getElementById("lblfullbtns").textContent += six
+}
+function inputone()
+{
+	one = document.getElementById("btnone").textContent;
+	document.getElementById("lblfullbtns").textContent += one
+}
+function inputtwo()
+{
+	two = document.getElementById("btntwo").textContent;
+	document.getElementById("lblfullbtns").textContent += two
+}
+function inputthree()
+{
+	three = document.getElementById("btnthree").textContent;
+	document.getElementById("lblfullbtns").textContent += three
+}
+function inputzero()
+{
+	zero = document.getElementById("btnzero").textContent;
+	document.getElementById("lblfullbtns").textContent += zero
+}
