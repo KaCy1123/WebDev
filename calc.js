c841cf20a37aa2c060f6aa8ddea8c6fd1db2c5a2
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2023-11-21 12:41:10 -0500

    updated 11/21/23

--- calc.js
+++ calc.js
@@ -1,74 +1,46 @@
 /**
  * 
  */
-window.addEventListener("load",addListener);
-
+window.addEventListener("load",initvar);
+function initvar()
+{
+	var input, numcreate, num1, num2, numfinal, operation;
+	
+	input = ""; //GET RID OF THIS SHIT STORE IT IN A LABEL FUCKER
+	numcreate = "";
+	num1 = "";
+	num2 = "";
+	numfinal = "";
+	operation = "";
+	addListener()
+}
 function addListener()
 {
 	document.getElementById("btnseven").addEventListener("click",inputseven);
 	document.getElementById("btneight").addEventListener("click",inputeight);
 	document.getElementById("btnnine").addEventListener("click",inputnine);
+	document.getElementById("btnadd").addEventListener("click",opadd);
 	document.getElementById("btnfour").addEventListener("click",inputfour);
 	document.getElementById("btnfive").addEventListener("click",inputfive);
 	document.getElementById("btnsix").addEventListener("click",inputsix);
+	document.getElementById("btnsub").addEventListener("click",opsub);
 	document.getElementById("btnone").addEventListener("click",inputone);
 	document.getElementById("btntwo").addEventListener("click",inputtwo);
 	document.getElementById("btnthree").addEventListener("click",inputthree);
+	document.getElementById("btnmult").addEventListener("click",opmult);
+	document.getElementById("btnsign").addEventListener("click",opsign);
 	document.getElementById("btnzero").addEventListener("click",inputzero);
+	document.getElementById("btndot").addEventListener("click",inputdot)
+	document.getElementById("btndiv").addEventListener("click",opdiv);
+	document.getElementById("btnequal").addEventListener("click",equal);
 	document.getElementById("btnclear").addEventListener("click",clear);
 }
 function inputseven()
 {
-	seven = document.getElementById("btnseven").textContent;
-	document.getElementById("lblfullbtns").textContent += seven
-}
-function inputeight()
-{
-	eight = document.getElementById("btneight").textContent;
-	document.getElementById("lblfullbtns").textContent += eight
-}
-function inputnine()
-{
-	nine = document.getElementById("btnnine").textContent;
-	document.getElementById("lblfullbtns").textContent += nine
-}
-function inputfour()
-{
-	four = document.getElementById("btnfour").textContent;
-	document.getElementById("lblfullbtns").textContent += four
-}
-function inputfive()
-{
-	five = document.getElementById("btnfive").textContent;
-	document.getElementById("lblfullbtns").textContent += five
-}
-function inputsix()
-{
-	six = document.getElementById("btnsix").textContent;
-	document.getElementById("lblfullbtns").textContent += six
-}
-function inputone()
-{
-	one = document.getElementById("btnone").textContent;
-	document.getElementById("lblfullbtns").textContent += one
-}
-function inputtwo()
-{
-	two = document.getElementById("btntwo").textContent;
-	document.getElementById("lblfullbtns").textContent += two
-}
-function inputthree()
-{
-	three = document.getElementById("btnthree").textContent;
-	document.getElementById("lblfullbtns").textContent += three
-}
-function inputzero()
-{
-	zero = document.getElementById("btnzero").textContent;
-	document.getElementById("lblfullbtns").textContent += zero
-}
-function clear()
-{
-	clear = document.getElementById("btnclear").textContent;
-	document.getElementById("lblfullbtns").textContent = ""
+	var input, numcreate;
+	
+	input = document.getElementById("btnseven").textContent;
+	numcreate += input;
+	alert(numcreate);
+	document.getElementById("lblfullbtns").textContent = numcreate;
 }
