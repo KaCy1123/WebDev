492cf976c6c1b45f542312e075a1caa5aeaffab4
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2023-12-17 16:09:50 -0500

    Update Js.js

--- In Class Quiz/public/Js.js
+++ In Class Quiz/public/Js.js
@@ -3,45 +3,78 @@ window.addEventListener("load",addListeners);
 function addListeners()
 {
 	document.getElementById("btnInput").disabled = true;
-	document.getElementById("inpNum").addEventListener("keypress",output);
+	document.getElementById("inpNum").addEventListener("keyup",numinp);
 	document.getElementById("btnInput").addEventListener("click",output);
+}
+
+function numinp()
+{
+	if (document.getElementById("inpNum").value == "")
+	{
+		document.getElementById("btnInput").disabled = true;
+		document.getElementById("lblOutput").textContent = "";
+	}
+	else{
+		document.getElementById("btnInput").disabled = false;
+	}
 }
 
 function output()
 {
-	var num1 = document.getElementById("inpNum").value;
-	var output = num1;
-	switch (num1){
-		case num1 > 20:	
-			document.getElementById("lblOutput").textContent = "";
-			alert("error: this is not an integer 6-20");
+	var num1 = document.getElementById("inpNum").value, 
+	output = num1, 
+	deciCheck = num1 % 1, 
+	modnum1 = num1%2;
+	switch (true){  
+	/// compares "true" to cases. If a case is true [Such as num1 > 20] it will read as case (true). 
+	/// switch(variable) works if i'm checking for a single value instead of comparison.
+		case (num1 > 20):	
+			document.getElementById("lblOutput").textContent = "error: this is not an integer 6-20";
 			break;
 
-		case num1 < 6:
-			document.getElementById("lblOutput").textContent = "";
-			alert("error: this is not an integer 6-20");
+		case (num1 < 6):
+			document.getElementById("lblOutput").textContent = "error: this is not an integer 6-20";
 			break;
 
-		case num1 % 1 != 0:
-			document.getElementById("lblOutput").textContent = "";
-			alert("error: this is a decimal");
+		case (deciCheck !== 0):
+			document.getElementById("lblOutput").textContent = "error: this is a decimal";
 			break;
 			
 		default:
-			modnum1 = num1%2
-			switch (){
-				case modnum1 = 0:
-					var EvenOdd = "even"
-					
+			modnum1 = output%2;
+			switch (modnum1){
+				case (modnum1 = 0):
+					var EvenOdd = "even";
+					break;
+				case (modnum1 = 1):
+					var EvenOdd = "odd";
+					break;
+				default:
+					var EvenOdd = "error";
+					break;
 			}
-			document.getElementById("lblOutput").textContent = output + "" + EvenOdd;
+			
+			document.getElementById("lblOutput").textContent = output + " " + EvenOdd;
 			output = output * 2;
 			
-			while (output < num1 * 32){			
+			while (output < num1 * 32){
+			modnum1 = output%2;
+			switch (modnum1){
+				case (modnum1 = 0):
+					var EvenOdd = "even";
+					break;
+				case (modnum1 = 1):
+					var EvenOdd = "odd";
+					break;
+				default:
+					var EvenOdd = "error";
+					break;
+			}
 				document.getElementById("lblOutput").textContent = 
-				document.getElementById("lblOutput").textContent + ", " + output + " " + "even";
+				document.getElementById("lblOutput").textContent + ", " + output + " " + EvenOdd;
 				output = output * 2;
 			}
 			break;
-	}
+			}
+			
 }
