f72e0f738d47fce6c3b81788e1d3a9ed046dbc8b
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2023-12-21 12:11:35 -0500

    Add files via upload

---
+++ Mini Project - Progress Report/public/indexjs.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", start);
+var UNAME = "";
+var PWORD = "";
+var StoredUNAME = "";
+var StoredPWORD = "";
+
+function start()
+{
+	document.getElementById("btnNextPage").disabled = true;
+	document.getElementById("btnSignup").addEventListener("click", Signup);
+	document.getElementById("btnLogin").addEventListener("click", Login);
+}
+
+function Signup()
+{
+	UNAME = document.getElementById("txtUsername").value;
+	PWORD = document.getElementById("txtPassword").value;
+	localStorage.setItem("Username", UNAME);
+	localStorage.setItem("Password", PWORD);
+	document.getElementById("lblErrorMsg").innerText = "Successfully signed up. Proceed to Login.";
+}
+
+function Login()
+{
+	let StoredUNAME = localStorage.getItem("Username");
+	let StoredPWORD = localStorage.getItem("Password");
+	UNAME = document.getElementById("txtUsername").value;
+	PWORD = document.getElementById("txtPassword").value;
+	if (UNAME !== StoredUNAME || PWORD !== StoredPWORD){
+		document.getElementById("lblErrorMsg").innerText = "Error";
+		alert("error")
+	}
+	else {
+		document.getElementById("lblErrorMsg").innerText = "Successfully logged in. Proceed to next page.";
+		document.getElementById("btnNextPage").disabled = false;
+	}
+}
