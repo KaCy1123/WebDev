b03be9115feeee47268e915c81e962a806a3ca13
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2023-12-22 12:34:20 -0500

    Add files via upload

--- Mini Project - Progress Report/public/page1js.js
+++ Mini Project - Progress Report/public/page1js.js
@@ -1,3 +1,10 @@
-/**
- * 
- */
+window.addEventListener("load",start);
+
+function start(){
+	document.getElementById("btnNext").disabled = true;
+	document.getElementById("txtPeriod8Friday").addEventListener("keypress",enablenext);
+}
+
+function enablenext(){
+	document.getElementById("btnSubmit").disabled = false;
+}
