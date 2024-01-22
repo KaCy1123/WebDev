fddec11e53d34a9e928de195b3ca9b96d9cd1d17
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2024-01-22 11:43:08 -0500

    adfdaf

---
+++ multipurpose website/public/mathscript.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", init);
+
+function init(){
+	document.getElementById("Q2").style.display = "none";
+	document.getElementById("Q3").style.display = "none";
+	document.getElementById("Q4").style.display = "none";
+	document.addEventListener("txtQ1")
+}
