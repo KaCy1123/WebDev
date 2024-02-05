377b748e054fa0039fda9175c0eb002446096cb7
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2024-02-05 12:42:20 -0500

    finish js :>

---
+++ multipurpose site/public/calculatorjs.js
@@ -0,0 +1,12 @@
+window.addEventListener("load",init);
+
+function init(){
+	alert()
+	document.getElementById("btnclear").addEventListener("click",clear);
+	document.getElementById("btnclear").addEventListener("click",clear);
+}
+
+function clear(){
+	document.getElementById("txtoutput").value = " "
+}
+
