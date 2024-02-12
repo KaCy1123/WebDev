ca605bdab9809ea20a31a778f967e720680646f6
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2024-02-11 21:49:11 -0500

    calculator completed

--- Multipurpose Website Final Cut/public/calculatorjs.js
+++ Multipurpose Website Final Cut/public/calculatorjs.js
@@ -18,6 +18,20 @@ function init(){
 	document.getElementById("btnnine").addEventListener("click",nine);
 	document.getElementById("btnexp").addEventListener("click",exp);
 	document.getElementById("btntimes").addEventListener("click",multiply);
+	document.getElementById("btnfour").addEventListener("click",four);
+	document.getElementById("btnfive").addEventListener("click",five);
+	document.getElementById("btnsix").addEventListener("click",six);
+	document.getElementById("btndeci").addEventListener("click",percent);
+	document.getElementById("btnminus").addEventListener("click",minus);
+	document.getElementById("btnone").addEventListener("click",one);
+	document.getElementById("btntwo").addEventListener("click",two);
+	document.getElementById("btnthree").addEventListener("click",three);
+	document.getElementById("btnabs").addEventListener("click",absolute);
+	document.getElementById("btnplus").addEventListener("click",add);
+	document.getElementById("btnzero").addEventListener("click",zero);
+	document.getElementById("btndot").addEventListener("click",dot);
+	document.getElementById("btnsign").addEventListener("click",sign);
+	document.getElementById("btnequal").addEventListener("click",equal);
 }
 
 function clear(){
@@ -29,24 +43,39 @@ function clear(){
 
 function sin(){
 	display = document.getElementById("txtoutput").value;
-	display = parseFloat(display);
-	display = Math.sin(display);
+	if (display == ""){
+		display = "";
+	}
+	else{
+		display = parseFloat(display);
+		display = Math.sin(display);
+	}
 	display = String(display);
 	document.getElementById("txtoutput").value = display;
 }
 
 function cos(){
 	display = document.getElementById("txtoutput").value;
-	display = parseFloat(display);
-	display = Math.cos(display);
+	if (display == ""){
+		display = "";
+	}
+	else{
+		display = parseFloat(display);
+		display = Math.cos(display);
+	}
 	display = String(display);
 	document.getElementById("txtoutput").value = display;
 }
 
 function tan(){
 	display = document.getElementById("txtoutput").value;
-	display = parseFloat(display);
-	display = Math.tan(display);
+	if (display == ""){
+		display = "";
+	}
+	else{
+		display = parseFloat(display);
+		display = Math.tan(display);
+	}
 	display = String(display);
 	document.getElementById("txtoutput").value = display;
 }
@@ -59,24 +88,39 @@ function del(){
 
 function e(){
 	display = document.getElementById("txtoutput").value;
-	display = parseFloat(display)
-	display = display * Math.E;
+	if (display == ""){
+		display = Math.E;
+	}
+	else{
+		display = parseFloat(display)
+		display = display * Math.E;
+	}
 	display = String(display) 
 	document.getElementById("txtoutput").value = display;
 }
 
 function sqrt(){
 	display = document.getElementById("txtoutput").value;
-	display = parseFloat(display);
-	display = Math.sqrt(display);
+	if (display == ""){
+		display = "";
+	}
+	else{
+		display = parseFloat(display);
+		display = Math.sqrt(display);
+	}
 	display = String(display);
 	document.getElementById("txtoutput").value = display;
 }
 
 function square(){
 	display = document.getElementById("txtoutput").value;
+	if (display == ""){
+		display = "";
+	}
+	else{
 	display = parseFloat(display);
 	display = display^2;
+	}
 	display = String(display);
 	document.getElementById("txtoutput").value = display;
 }
@@ -94,35 +138,36 @@ function divide(){
 	else{
 		switch(operator){
 			case "+":
-				display = display + num1;
+				display = num1 + display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 
 			case "-":
-				display = display - num1;
+				display = num1 - display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 				
 			case "/":
-				display = display / num1;
+				display = num1 / display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 				
 			case "*":
-				display = display * num1;
+				display = num1 * display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 			
 			case "^":
-				display = display ^ num1;
+				display = num1 ^ display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 				
 			default:
-				num1 = display;
-				operator = "^";
-				display = "";
-				document.getElementById("txtoutput").value = display;
 				break;
 		}
 		operator = ""
@@ -133,21 +178,18 @@ function divide(){
 
 function seven(){
 	display = document.getElementById("txtoutput").value;
-	display = display;
 	display = display + "7";
 	document.getElementById("txtoutput").value = display;
 }
 
 function eight(){
 	display = document.getElementById("txtoutput").value;
-	display = display;
 	display = display + "8";
 	document.getElementById("txtoutput").value = display;
 }
 
 function nine(){
 	display = document.getElementById("txtoutput").value;
-	display = display;
 	display = display + "9";
 	document.getElementById("txtoutput").value = display;
 }
@@ -165,35 +207,36 @@ function exp(){
 	else{
 		switch(operator){
 			case "+":
-				display = display + num1;
+				display = num1 + display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 
 			case "-":
-				display = display - num1;
+				display = num1 - display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 				
 			case "/":
-				display = display / num1;
+				display = num1 / display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 				
 			case "*":
-				display = display * num1;
+				display = num1 * display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 			
 			case "^":
-				display = display ^ num1;
+				display = num1 ^ display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 				
 			default:
-				num1 = display;
-				operator = "^";
-				display = "";
-				document.getElementById("txtoutput").value = display;
 				break;
 		}
 		operator = ""
@@ -214,35 +257,279 @@ function multiply(){
 	else{
 		switch(operator){
 			case "+":
-				display = display + num1;
+				display = num1 + display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 
 			case "-":
-				display = display - num1;
+				display = num1 - display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 				
 			case "/":
-				display = display / num1;
+				display = num1 / display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 				
 			case "*":
-				display = display * num1;
+				display = num1 * display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 			
 			case "^":
-				display = display ^ num1;
+				display = num1 ^ display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
 				break;
 				
 			default:
-				num1 = display;
-				operator = "*";
-				display = "";
+				break;
+		}
+		operator = ""
+		num1 = ""
+	}
+}
+
+function four(){
+	display = document.getElementById("txtoutput").value;
+	display = display + "4";
+	document.getElementById("txtoutput").value = display;
+}
+
+function five(){
+	display = document.getElementById("txtoutput").value;
+	display = display + "5";
+	document.getElementById("txtoutput").value = display;
+}
+
+function six(){
+	display = document.getElementById("txtoutput").value;
+	display = display + "6";
+	document.getElementById("txtoutput").value = display;
+}
+
+function percent(){
+	display = document.getElementById("txtoutput").value;
+	if (display == ""){
+		display = "";
+	}
+	else{
+		display = parseFloat(display)
+		display = display * .01;
+	}
+	display = String(display) 
+	document.getElementById("txtoutput").value = display;
+}
+
+function minus(){
+	display = document.getElementById("txtoutput").value;
+	display = parseFloat(display);
+	
+	if (operator == ""){
+		num1 = display;
+		operator = "-";
+		display = "";
+		document.getElementById("txtoutput").value = display;
+	}
+	else{
+		switch(operator){
+			case "+":
+				display = num1 + display;
+				display = String(display);
 				document.getElementById("txtoutput").value = display;
+				break;
+
+			case "-":
+				display = num1 - display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+				
+			case "/":
+				display = num1 / display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+				
+			case "*":
+				display = num1 * display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+			
+			case "^":
+				display = num1 ^ display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+				
+			default:
+				break;
+		}
+		operator = ""
+		num1 = ""
+	}
+}
+
+function one(){
+	display = document.getElementById("txtoutput").value;
+	display = display + "1";
+	document.getElementById("txtoutput").value = display;
+}
+
+function two(){
+	display = document.getElementById("txtoutput").value;
+	display = display + "2";
+	document.getElementById("txtoutput").value = display;
+}
+
+function three(){
+	display = document.getElementById("txtoutput").value;
+	display = display + "3";
+	document.getElementById("txtoutput").value = display;
+}
+
+function absolute(){
+	display = document.getElementById("txtoutput").value;
+	var sign = display.slice(0,1);
+	switch(sign){
+		case "":
+			display = ""
+			break;
+		
+		case "-":
+			display = parseFloat(display);
+			display = display * -1
+			break
+		
+		default:
+			display = display;
+			break
+	}
+	display = String(display) 
+	document.getElementById("txtoutput").value = display;
+}
+
+function add(){
+	display = document.getElementById("txtoutput").value;
+	display = parseFloat(display);
+	
+	if (operator == ""){
+		num1 = display;
+		operator = "+";
+		display = "";
+		document.getElementById("txtoutput").value = display;
+	}
+	else{
+		switch(operator){
+			case "+":
+				display = num1 + display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+
+			case "-":
+				display = num1 - display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+				
+			case "/":
+				display = num1 / display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+				
+			case "*":
+				display = num1 * display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+			
+			case "^":
+				display = num1 ^ display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+				
+			default:
+				break;
+		}
+		operator = ""
+		num1 = ""
+	}
+}
+
+function zero(){
+	display = document.getElementById("txtoutput").value;
+	display = display + "0";
+	document.getElementById("txtoutput").value = display;
+}
+
+function dot(){
+	display = document.getElementById("txtoutput").value;
+	display = display + ".";
+	document.getElementById("txtoutput").value = display;
+}
+
+function sign(){
+	display = document.getElementById("txtoutput").value;
+	if (display == ""){
+		display = "";
+	}
+	else{
+		display = parseFloat(display)
+		display = display * -1;
+	}
+	display = String(display) 
+	document.getElementById("txtoutput").value = display;
+}
+
+function equal(){
+	display = document.getElementById("txtoutput").value;
+	display = parseFloat(display);
+	
+	if (operator == ""){
+		display = String(display);
+		document.getElementById("txtoutput").value = display;
+	}
+	else{
+		switch(operator){
+			case "+":
+				display = num1 + display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+
+			case "-":
+				display = num1 - display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+				
+			case "/":
+				display = num1 / display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+				
+			case "*":
+				display = num1 * display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+			
+			case "^":
+				display = num1 ^ display;
+				display = String(display);
+				document.getElementById("txtoutput").value = display;
+				break;
+				
+			default:
 				break;
 		}
 		operator = ""
